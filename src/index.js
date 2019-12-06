5ddd54f9f51aeaff33211b56c4e962c5f75b39fc
James Kahng <jameskahng@gmail.com>
2019-12-05 16:29:33 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// import dependencies
+import RankGenerator from './RankGenerator'
+
+// output top patients to contact given a location
+let dataPath = "./sample-data/patients.json";
+let location = {
+    "latitude": "68.8129",
+    "longitude": "71.3018"
+}
+const rankGenerator = new RankGenerator();
+rankGenerator.loadPatientData(dataPath);
+
+console.log(rankGenerator.fetchBestPatients(location));
