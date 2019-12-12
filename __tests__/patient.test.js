13461e44caf0e0b5a4ca7fa5402f377beb4554ea
Raphael Cleto <raphael.garcia.cleto@live.com>
2019-12-12 19:54:39 -0300

    Adding patient entity tests

---
+++ __tests__/patient.test.js
@@ -0,0 +1,59 @@
+const request = require('supertest');
+const faker = require('faker');
+const app = require('../src/App');
+
+const facilityLocation = {
+  facilityLocation: {
+    latitude: faker.address.latitude(),
+    longitude: faker.address.longitude(),
+  },
+};
+
+describe('Patient', () => {
+  it('should be able to return top 10 patients', async () => {
+    const response = await request(app)
+      .get('/patients/rank')
+      .send({ ...facilityLocation });
+
+    expect(response.body).toHaveLength(10);
+  });
+
+  it('should return 400 status code if facilityLocation is missing', async () => {
+    const response = await request(app)
+      .get('/patients/rank')
+      .send();
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should return 400 status code if patientsQty is less than 10', async () => {
+    const response = await request(app)
+      .get('/patients/rank')
+      .send({
+        ...facilityLocation,
+        patientsQty: faker.random.number({ max: 9, min: 0 }),
+      });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should return at least one patient with score 10', async () => {
+    const response = await request(app)
+      .get('/patients/rank')
+      .send({ ...facilityLocation });
+
+    const firstPatientScore = response.body[0].score;
+
+    expect(firstPatientScore).toBe(10);
+  });
+
+  it('should return at least one patient with score 1', async () => {
+    const response = await request(app)
+      .get('/patients/rank')
+      .send({ ...facilityLocation });
+
+    const firstPatientScore = response.body[9].score;
+
+    expect(firstPatientScore).toBe(1);
+  });
+});
