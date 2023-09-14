4e5cfa93d5ee3e9ee3285aad6a7b9e70396a8441
mutai-gilbert <mutaigilbert85@gmail.com>
2023-09-14 21:24:54 +0300

    Add new files on the server-side and the client-side

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     fetch('/patients')
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error(`Network response was not ok: ${response.status}`);
-//         }
-//         return response.json();
-//     })
-//     .then(data => {
-//         if (!Array.isArray(data)) {
-//             throw new Error('Response data is not an array.');
-//         }
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('/patients')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (!Array.isArray(data)) {
+            throw new Error('Response data is not an array.');
+        }
 
-//         data.forEach(patient => {
-//             patient.score = computePatientScore(patient);
-//         });
-//         displayTopPatients(data);
-//     })
-//     .catch(error => {
-//         console.error('Error fetching or processing data: ', error);
-//     });
-// });
+        data.forEach(patient => {
+            patient.score = computePatientScore(patient);
+        });
+        displayTopPatients(data);
+    })
+    .catch(error => {
+        console.error('Error fetching or processing data: ', error);
+    });
+});
 
 function displayTopPatients(patients){
     const patientList = document.getElementById('patientList');
