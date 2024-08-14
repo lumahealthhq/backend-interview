1e8c56e25a22a1c1a8e7cf5abc7b2ab77f6536a7
Laian <braumlaian@gmail.com>
2024-08-14 15:33:22 -0300

    test: add tests for all services

---
+++ tests/main/services/patient-data-min-max-calculator-service.spec.ts
@@ -0,0 +1,100 @@
+import { PatientDataMinMaxCalculatorService } from "../../../src/data/services";
+
+const makeSut = () => {
+  return new PatientDataMinMaxCalculatorService();
+};
+
+describe("PatientDataMinMaxCalculatorService", () => {
+  it("Should calculate min and max values for patient fields", () => {
+    const sut = makeSut();
+
+    const maxAge = 50;
+    const minAge = 10;
+    const maxAcceptedOffers = 40;
+    const minAcceptedOffers = 5;
+    const maxCanceledOffers = 250;
+    const minCanceledOffers = 20;
+    const maxAvgReplyTime = 3000;
+    const minAvgReplyTime = 100;
+
+    const patients = [
+      {
+        name: "Samir Pacocha",
+        age: maxAge,
+        acceptedOffers: maxAcceptedOffers,
+        canceledOffers: minCanceledOffers,
+        averageReplyTime: 2000,
+      },
+      {
+        name: "Bernard Mosciski",
+        age: 20,
+        acceptedOffers: 10,
+        canceledOffers: maxCanceledOffers,
+        averageReplyTime: maxAvgReplyTime,
+      },
+      {
+        name: "Theo Effertz",
+        age: minAge,
+        canceledOffers: 90,
+        acceptedOffers: minAcceptedOffers,
+        averageReplyTime: minAvgReplyTime,
+      },
+    ];
+
+    const { acceptedOffers, age, averageReplyTime, canceledOffers } =
+      sut.calculate(patients as Patient[]);
+
+    expect(age).toEqual({ max: maxAge, min: minAge });
+    expect(averageReplyTime).toEqual({
+      max: maxAvgReplyTime,
+      min: minAvgReplyTime,
+    });
+    expect(canceledOffers).toEqual({
+      max: maxCanceledOffers,
+      min: minCanceledOffers,
+    });
+    expect(acceptedOffers).toEqual({
+      max: maxAcceptedOffers,
+      min: minAcceptedOffers,
+    });
+  });
+
+  it("Should return default values as min and max when default values are both lower and higher than values from provided patients", () => {
+    const sut = makeSut();
+
+    const defaultValuesMock = {
+      age: {
+        min: Number.MIN_SAFE_INTEGER,
+        max: Number.MAX_SAFE_INTEGER,
+      },
+      acceptedOffers: {
+        min: Number.MIN_SAFE_INTEGER,
+        max: Number.MAX_SAFE_INTEGER,
+      },
+      canceledOffers: {
+        min: Number.MIN_SAFE_INTEGER,
+        max: Number.MAX_SAFE_INTEGER,
+      },
+      averageReplyTime: {
+        min: Number.MIN_SAFE_INTEGER,
+        max: Number.MAX_SAFE_INTEGER,
+      },
+    };
+
+    sut.defaultValues = defaultValuesMock;
+
+    const patients = [
+      {
+        name: "Kevin Malone",
+        age: 20,
+        acceptedOffers: 10,
+        canceledOffers: 10,
+        averageReplyTime: 500,
+      },
+    ];
+
+    const minMaxValues = sut.calculate(patients as Patient[]);
+
+    expect(minMaxValues).toEqual(defaultValuesMock);
+  });
+});
---
+++ tests/main/services/patient-score-calculator-service.spec.ts
@@ -0,0 +1,152 @@
+import { PatientScoreCalculatorService } from "../../../src/data/services";
+
+import { PatientDataNormalizerSpy } from "./mocks";
+
+const makeSut = () => {
+  const normalizerSpy = new PatientDataNormalizerSpy();
+  const sut = new PatientScoreCalculatorService(normalizerSpy);
+
+  return {
+    sut,
+    normalizerSpy,
+  } as const;
+};
+
+describe("PatientScoreCalculatorService", () => {
+  describe("Calculates a score from 0 to 1 (worst to best) based on field normalization and weight", () => {
+    const minMax = {
+      age: { max: 50, min: 10 },
+      acceptedOffers: { max: 95, min: 5 },
+      canceledOffers: { max: 250, min: 20 },
+      averageReplyTime: { max: 3000, min: 100 },
+    };
+
+    describe("Default Weight", () => {
+      it("Should return a high score if normalized results are closer to 1", () => {
+        const { sut, normalizerSpy } = makeSut();
+
+        const patient = {
+          name: "Michael Scott",
+          distance: 1,
+          age: 16,
+          acceptedOffers: 95,
+          canceledOffers: 20,
+          averageReplyTime: 100,
+        };
+
+        normalizerSpy.normalizeResult = {
+          age: 0.8,
+          distance: 0.8,
+          acceptedOffers: 0.8,
+          averageReplyTime: 0.8,
+          canceledOffers: 0.8,
+        };
+
+        const { score, littleBehaviorScore } = sut.calculate(
+          patient as Required<Patient>,
+          minMax
+        );
+
+        expect(score).toBeCloseTo(1, 0);
+        expect(littleBehaviorScore).toBeCloseTo(0.6, 0);
+      });
+
+      it("Should return a terrible score if normalized results are closer to 0", () => {
+        const { sut, normalizerSpy } = makeSut();
+
+        const patient = {
+          name: "Dwight Kurt Schrute",
+          distance: 1000,
+          age: 50,
+          acceptedOffers: 5,
+          canceledOffers: 250,
+          averageReplyTime: 3000,
+        };
+
+        normalizerSpy.normalizeResult = {
+          acceptedOffers: 0.1,
+          age: 0.1,
+          averageReplyTime: 0.1,
+          canceledOffers: 0.1,
+          distance: 0.1,
+        };
+
+        const { score, littleBehaviorScore } = sut.calculate(
+          patient as Required<Patient>,
+          minMax
+        );
+
+        expect(score).toBeCloseTo(0, 0);
+        expect(littleBehaviorScore).toBeCloseTo(0, 0);
+      });
+
+      it("Should return a mediocre score if normalized results are close to 0.5", () => {
+        const { sut, normalizerSpy } = makeSut();
+
+        const patient = {
+          name: "Ryan Howard",
+          distance: 50,
+          age: 30,
+          acceptedOffers: 50,
+          canceledOffers: 135,
+          averageReplyTime: 1550,
+        };
+
+        normalizerSpy.normalizeResult = {
+          acceptedOffers: 0.5,
+          age: 0.5,
+          averageReplyTime: 0.5,
+          canceledOffers: 0.5,
+          distance: 0.5,
+        };
+
+        const { score, littleBehaviorScore } = sut.calculate(
+          patient as Required<Patient>,
+          minMax
+        );
+
+        expect(score).toBeCloseTo(0.5, 0);
+        expect(littleBehaviorScore).toBe(0.4);
+      });
+    });
+
+    describe("Custom Weight", () => {
+      it("Should return a perfect score due to high age WEIGHT and terrible little behavior score due to poor results on behavior values", () => {
+        const { sut, normalizerSpy } = makeSut();
+
+        sut.WEIGHT = {
+          age: 1,
+          distance: 0,
+          acceptedOffers: 0,
+          canceledOffers: 0,
+          replyTime: 0,
+        };
+
+        normalizerSpy.normalizeResult = {
+          age: 1,
+          distance: 0,
+          acceptedOffers: 0,
+          averageReplyTime: 0,
+          canceledOffers: 0,
+        };
+
+        const patient = {
+          name: "Michael Scott",
+          distance: 1,
+          age: 16,
+          acceptedOffers: 95,
+          canceledOffers: 20,
+          averageReplyTime: 100,
+        };
+
+        const { score, littleBehaviorScore } = sut.calculate(
+          patient as Required<Patient>,
+          minMax
+        );
+
+        expect(score).toBeCloseTo(1, 0);
+        expect(littleBehaviorScore).toBe(0);
+      });
+    });
+  });
+});
