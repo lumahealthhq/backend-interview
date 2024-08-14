50a3cf3109526c613d42f3a9c781d99cf56edb77
Laian <braumlaian@gmail.com>
2024-08-14 16:46:02 -0300

    refactor: delete lib/ and its tests

--- src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./normalize";
-export * from "./calculate-min-max-values";
-export * from "./calculate-patient-score";
-export * from "./get-patients-top-list";
-export * from "./get-patients-with-scores";
