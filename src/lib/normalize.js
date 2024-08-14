a9bf433903eb2c3295bea444f1a779608a594ed0
Laian <braumlaian@gmail.com>
2024-08-13 23:27:51 -0300

    refactor: normalize:  stop handling greater/less than scenarios because values must always be greater than min and less than max

--- src/lib/normalize.js
+++ src/lib/normalize.js
@@ -1,5 +1,5 @@
 /**
- * @param {number} value
+ * @param {number} value Must be greater than min and less than max
  * @param {number} min
  * @param {number} max
  * @returns {number} A number between 0 and 1, representing how close "value" is to either "max" or "min".
@@ -7,9 +7,6 @@
 module.exports.normalize = (value, min, max) => {
   // ? Avoid division by 0. Returning 0.5 because it's common ground.
   if (max === min) return 0.5;
-
-  if (value > max) return 1;
-  if (value < min) return 0;
 
   return (value - min) / (max - min);
 };
--- src/lib/normalize.spec.js
+++ src/lib/normalize.spec.js
@@ -19,26 +19,18 @@ describe("normalize", () => {
   });
 
   describe("Return values between 0 and 1", () => {
-    it("Should return 0 when value is less or equal than min", () => {
-      const cases = [
-        { value: 10, min: 10, max: 50 },
-        { value: 5, min: 10, max: 50 },
-      ];
-
-      cases.forEach((x) => {
-        expect(normalize(x.value, x.min, x.max)).toBe(0);
-      });
+    it("Should return 0 when value is equal min", () => {
+      const value = -100;
+      const min = -100;
+      const max = 100;
+      expect(normalize(value, min, max)).toBe(0);
     });
 
-    it("Should return 1 when value is greater or equal than max", () => {
-      const cases = [
-        { value: 0, min: -100, max: 0 },
-        { value: 1, min: -100, max: 0 },
-      ];
-
-      cases.forEach((x) => {
-        expect(normalize(x.value, x.min, x.max)).toBe(1);
-      });
+    it("Should return 1 when value is equal max", () => {
+      const value = 99;
+      const min = 0;
+      const max = 99;
+      expect(normalize(value, min, max)).toBe(1);
     });
 
     it("Should return closer to 1 when value is closer to max", () => {
