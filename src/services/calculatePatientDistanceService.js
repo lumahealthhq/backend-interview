a7141cf8286d7068aa2971f208723cf57ea06a82
Victor Fernandes <victor.hmfd@gmail.com>
2024-08-18 09:46:14 -0300

    fix: location params validation

--- src/services/calculatePatientDistanceService.js
+++ src/services/calculatePatientDistanceService.js
@@ -3,13 +3,13 @@ import { getDistance } from 'geolib';
 class CalculatePatientDistanceService {
   execute({ patients, facilityLatitude, facilityLongitude }) {
     this._validateParams({
-      facilityLatitude: Number(facilityLatitude),
-      facilityLongitude: Number(facilityLongitude)
+      facilityLatitude: facilityLatitude,
+      facilityLongitude: facilityLongitude
     })
 
     return patients.map(patient => {
       const facilityDistance = getDistance(
-        { latitude: Number(facilityLatitude), longitude: Number(facilityLongitude) },
+        { latitude: facilityLatitude, longitude: facilityLongitude },
         { latitude: patient.location.latitude, longitude: patient.location.longitude }
       )
 
@@ -21,17 +21,28 @@ class CalculatePatientDistanceService {
   }
 
   _validateParams({ facilityLatitude, facilityLongitude }) {
-    if (!facilityLatitude || !facilityLongitude) {
-      throw new Error('Facility location params is missing')
+    if (!this._paramIsMissing(facilityLatitude)) {
+      throw new Error('Latitude param is missing');
     }
 
-    if (typeof facilityLatitude !== 'number' || facilityLatitude < -90 || facilityLatitude > 90) {
+    if (!this._paramIsMissing(facilityLongitude)) {
+      throw new Error('Longitude param is missing');
+    }
+
+    const latitudeNumber = Number(facilityLatitude)
+    const longitudeNumber = Number(facilityLongitude)
+
+    if (isNaN(latitudeNumber) || latitudeNumber < -90 || latitudeNumber > 90) {
       throw new Error(`Invalid latitude value. It must be a number between -90 and 90.`);
     }
 
-    if (typeof facilityLongitude !== 'number' || facilityLongitude < -180 || facilityLongitude > 180) {
+    if (isNaN(longitudeNumber) || longitudeNumber < -180 || longitudeNumber > 180) {
       throw new Error(`Invalid longitude value. It must be a number between -180 and 180.`);
     }
+  }
+
+  _paramIsMissing(value) {
+    return value !== '' && value !== null && value !== undefined;
   }
 }
 
--- src/services/calculatePatientDistanceService.spec.js
+++ src/services/calculatePatientDistanceService.spec.js
@@ -37,24 +37,47 @@ describe('CalculatePatientDistanceService', () => {
       expect(result[0].facilityDistance).toEqual(mockDistance)
     });
 
-    it('should throw an error if facilityLatitude or facilityLongitude is missing', () => {
+    it('should throw an error if facilityLatitude', () => {
       expect(() => service._validateParams({ facilityLatitude: null, facilityLongitude: -122.4194 }))
-        .toThrow('Facility location params is missing');
-      expect(() => service._validateParams({ facilityLatitude: 37.7749, facilityLongitude: undefined }))
-        .toThrow('Facility location params is missing');
+        .toThrow('Latitude param is missing');
+
+      expect(() => service._validateParams({ facilityLatitude: '', facilityLongitude: -122.4194 }))
+        .toThrow('Latitude param is missing');
+
+      expect(() => service._validateParams({ facilityLatitude: undefined, facilityLongitude: -122.4194 }))
+        .toThrow('Latitude param is missing');
     });
 
-    it('should throw an error if facilityLatitude is out of range', () => {
+    it('should throw an error if facilityLongitude is missing', () => {
+      expect(() => service._validateParams({ facilityLatitude: 37.7749, facilityLongitude: '' }))
+        .toThrow('Longitude param is missing');
+
+      expect(() => service._validateParams({ facilityLatitude: 37.7749, facilityLongitude: null }))
+        .toThrow('Longitude param is missing');
+
+      expect(() => service._validateParams({ facilityLatitude: 37.7749, facilityLongitude: undefined }))
+        .toThrow('Longitude param is missing');
+    });
+
+    it('should throw an error if facilityLatitude is out of range or is NaN', () => {
       expect(() => service._validateParams({ facilityLatitude: 100, facilityLongitude: -122.4194 }))
         .toThrow('Invalid latitude value. It must be a number between -90 and 90.');
+
       expect(() => service._validateParams({ facilityLatitude: -100, facilityLongitude: -122.4194 }))
+        .toThrow('Invalid latitude value. It must be a number between -90 and 90.');
+
+      expect(() => service._validateParams({ facilityLatitude: 'invalid', facilityLongitude: -122.4194 }))
         .toThrow('Invalid latitude value. It must be a number between -90 and 90.');
     });
 
-    it('should throw an error if facilityLongitude is out of range', () => {
+    it('should throw an error if facilityLongitude is out of range or is NaN', () => {
       expect(() => service._validateParams({ facilityLatitude: 37.7749, facilityLongitude: 200 }))
         .toThrow('Invalid longitude value. It must be a number between -180 and 180.');
+
       expect(() => service._validateParams({ facilityLatitude: 37.7749, facilityLongitude: -200 }))
+        .toThrow('Invalid longitude value. It must be a number between -180 and 180.');
+
+      expect(() => service._validateParams({ facilityLatitude: 37.7749, facilityLongitude: 'invalid' }))
         .toThrow('Invalid longitude value. It must be a number between -180 and 180.');
     });
   });
