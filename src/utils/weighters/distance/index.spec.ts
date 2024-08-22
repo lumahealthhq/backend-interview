a0154c9da6d2b2804bfeed66e5c64c6dcdb5e9e3
thassiov <tvmcarvalho@gmail.com>
2024-08-22 00:09:52 -0300

    Adds unit test for the weighter by distance

---
+++ src/utils/weighters/distance/index.spec.ts
@@ -0,0 +1,65 @@
+import { byDistance } from '.';
+import { TPatientRecordWithScore } from '../../../types/patient-record';
+
+describe('weighters: distance', () => {
+  it('should weight a list of records by their distance to a given point', () => {
+    const records = [
+      {
+        location: {
+          longitude: 1,
+          latitude: 1,
+        },
+        score: 0,
+      },
+      {
+        location: {
+          longitude: 7,
+          latitude: 5,
+        },
+        score: 0,
+      },
+      {
+        location: {
+          longitude: 9,
+          latitude: 9,
+        },
+        score: 0,
+      },
+      {
+        location: {
+          longitude: 11,
+          latitude: 15,
+        },
+        score: 0,
+      },
+      {
+        location: {
+          longitude: 11,
+          latitude: 16,
+        },
+        score: 0,
+      },
+    ];
+
+    const result = byDistance(records as unknown as TPatientRecordWithScore[], {
+      latitude: 0,
+      longitude: 0,
+    });
+
+    console.log(result);
+
+    expect((result[0] as TPatientRecordWithScore).score).toBeGreaterThan(
+      (result[1] as TPatientRecordWithScore).score
+    );
+    expect((result[1] as TPatientRecordWithScore).score).toBeGreaterThan(
+      (result[2] as TPatientRecordWithScore).score
+    );
+    expect((result[2] as TPatientRecordWithScore).score).toBeGreaterThan(
+      (result[3] as TPatientRecordWithScore).score
+    );
+    expect((result[3] as TPatientRecordWithScore).score).toBeGreaterThan(
+      (result[4] as TPatientRecordWithScore).score
+    );
+    expect((result[4] as TPatientRecordWithScore).score).toBe(0);
+  });
+});
